db08a44413dd0090591e84ce26f6fad32950dd37
divyan614 <divyanarayanan614@gmail.com>
2026-03-09 15:19:34 +0530

    UC3 - Reverse String

---
+++ app/src/PalindromeCheckerApp.java
@@ -0,0 +1,17 @@
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "madam";
+        String reversed = "";
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
+        }
+
+        boolean isPalindrome = input.equals(reversed);
+
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
+    }
+}
